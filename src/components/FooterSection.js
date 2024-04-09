6133d708b6603ce9828757dfe7d2f1c6eaef1754
nftmoondogs <nftmoondogs@gmail.com>
2024-04-09 06:22:52 +0530

    first commit

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -33,7 +33,7 @@ const FooterSection = () => {
               <a href="https://twitter.com/bitmonsoltoken" target="_blank" rel="noopener noreferrer" aria-label="Twitter" className="transition duration-300 ease-in-out hover:scale-125">
                 <span>Twitter</span> {/* Consider replacing <span> with an actual icon */}
               </a>
-              <a href="https://t.me/bitmonbitcommunity" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="transition duration-300 ease-in-out hover:scale-125">
+              <a href="https://t.me/SecretSocietyCult" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="transition duration-300 ease-in-out hover:scale-125">
                 <span>Telegram</span> {/* Consider replacing <span> with an actual icon */}
               </a>
             </div>
